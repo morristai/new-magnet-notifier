2d78f0d2134f2db218246751f36cfbe50c344aff
Morris Tai <morristai01@gmail.com>
2021-12-26 00:21:01 +0800

    feat: add local cache handler

---
+++ parser/cache.go
@@ -0,0 +1,50 @@
+package parser
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"os"
+	"sync"
+)
+
+var lock sync.Mutex
+
+var Marshal = func(v interface{}) (io.Reader, error) {
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(b), nil
+}
+
+var Unmarshal = func(r io.Reader, v interface{}) error {
+	return json.NewDecoder(r).Decode(v)
+}
+
+func Save(path string, v interface{}) error {
+	lock.Lock()
+	defer lock.Unlock()
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	r, err := Marshal(v)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(f, r)
+	return err
+}
+
+func Load(path string, v interface{}) error {
+	lock.Lock()
+	defer lock.Unlock()
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return Unmarshal(f, v)
+}
