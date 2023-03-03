3dedd993cf27dc3d193884f25178f122ffa869dc
Morris Tai <morristai01@gmail.com>
2023-03-03 11:39:06 -0500

    feat: modify for running on replit

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/morristai/rarbg-notifier/discord"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/morristai/rarbg-notifier/discord"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
 
 func main() {
-	discord.Run()
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		http.HandleFunc("/", hello)
+		http.ListenAndServe(":8090", nil)
+	}()
+
+	go func() {
+		defer wg.Done()
+		go discord.Run()
+	}()
+
+	wg.Wait()
 }
